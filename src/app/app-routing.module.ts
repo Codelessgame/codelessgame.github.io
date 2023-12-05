4fd1069faac9b77874f3b069c4aac1acc6fddd52
Stanislav Růžička <stanik.ruzicka@gmail.com>
2023-12-05 19:40:03 +0100

    progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './pages/home/home.component';
 
-
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'w', component: HomeComponent},
+  {path:'p', component: HomeComponent}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AppRoutingModule} from "./app-routing.module";
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SkillsComponent } from './layouts/skills/skills.component';
+import { AboutMeComponent } from './layouts/about-me/about-me.component';
+import { MyWorkComponent } from './layouts/my-work/my-work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SkillsComponent,
+    AboutMeComponent,
+    MyWorkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
