12866ce9c037d7ee206b0b01f5410e0d8ab19f4a
Stanislav Růžička <stanik.ruzicka@gmail.com>
2024-01-28 18:39:16 +0100

    Ui chnage , 3D link works, fixed text height line issue, fixed scaling IB cards issue needs blog content

--- src/app/pages/blog/blog.service.ts
+++ src/app/pages/blog/blog.service.ts
@@ -11,7 +11,7 @@ export class BlogService {
     {id: 2, title: 'Trip to a fusion reactor', image:'assets/tokamak.jpg', content: 'On the 31st of June I with a group of people set out within a school action " " ', side: "left"},
     {id: 3, title: 'Jaderne dny', image:'assets/code_img.jpg', content: 'test', side: "right"},
     {id: 4, title: 'Nobeluv institut', image:'assets/code_img.jpg', content: 'test', side: "right"},
-    {id: 5, title: 'vytvarny kurz 2023', image:'assets/code_img.jpg', content: 'test', side: "right"},
+    {id: 5, title: 'Vytvarny kurz 2023', image:'assets/code_img.jpg', content: 'test', side: "right"},
   ]
 
   getAllPosts(): BlogPostData[] {
