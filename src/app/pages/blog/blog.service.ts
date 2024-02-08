fdcb691cb4f0d1fc8888a102a31a8c5a82a77521
Stanislav Růžička <stanik.ruzicka@gmail.com>
2024-02-08 20:00:42 +0100

    text height line

--- src/app/pages/blog/blog.service.ts
+++ src/app/pages/blog/blog.service.ts
@@ -11,9 +11,11 @@ export class BlogService {
     {id: 2, title: 'Trip to a fusion reactor', image:'assets/tokamak.jpg', content: '' +
         'On the 31st of June I with a group of people set out within a school action " " ' +
         '', side: "left"},
-    {id: 3, title: 'Jaderne dny', image:'assets/code_img.jpg', content: 'test', side: "right"},
-    {id: 4, title: 'Nobeluv institut', image:'assets/nobeluv_institut.jpg', content: 'test', side: "right"},
-    {id: 5, title: 'Vytvarny kurz 2023', image:'assets/art_course.jpg', content: 'test', side: "right"},
+    {id: 3, title: 'Nuclear days', image:'assets/code_img.jpg', content: 'test', side: "right"},
+    {id: 4, title: 'Nobel`s institute', image:'assets/nobeluv_institut.jpg', content: 'test', side: "right"},
+    {id: 5, title: 'Art camp 2023', image:'assets/art_course.jpg', content: 'test', side: "right"},
+    {id: 6, title: 'App Decibel', image:'assets/fanda.svg', content: 'This is an application programed by me and my brother as a joke to a friend the sounds were edited by me the working app can be found here https://decibel.bronaruzicka.cz/.', side: "left"},
+
   ]
 
   getAllPosts(): BlogPostData[] {
