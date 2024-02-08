a303f1a026573e8cc9a0cbda8428a317d617927a
Stanislav Růžička <stanik.ruzicka@gmail.com>
2024-02-08 19:40:20 +0100

    text height line

--- src/app/pages/blog/blog.service.ts
+++ src/app/pages/blog/blog.service.ts
@@ -7,11 +7,13 @@ import {BlogPostData} from "./blogpostdata";
 export class BlogService {
 
   posts: BlogPostData[] = [
-    {id: 1, title: 'Ichep 2024', image:'assets/code_img.jpg', content: 'test', side: "right"},
-    {id: 2, title: 'Trip to a fusion reactor', image:'assets/tokamak.jpg', content: 'On the 31st of June I with a group of people set out within a school action " " ', side: "left"},
+    {id: 1, title: 'Ichep 2024', image:'assets/Ichep_1.jpg', content: 'test', side: "right"},
+    {id: 2, title: 'Trip to a fusion reactor', image:'assets/tokamak.jpg', content: '' +
+        'On the 31st of June I with a group of people set out within a school action " " ' +
+        '', side: "left"},
     {id: 3, title: 'Jaderne dny', image:'assets/code_img.jpg', content: 'test', side: "right"},
-    {id: 4, title: 'Nobeluv institut', image:'assets/code_img.jpg', content: 'test', side: "right"},
-    {id: 5, title: 'Vytvarny kurz 2023', image:'assets/code_img.jpg', content: 'test', side: "right"},
+    {id: 4, title: 'Nobeluv institut', image:'assets/nobeluv_institut.jpg', content: 'test', side: "right"},
+    {id: 5, title: 'Vytvarny kurz 2023', image:'assets/art_course.jpg', content: 'test', side: "right"},
   ]
 
   getAllPosts(): BlogPostData[] {
